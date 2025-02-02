20424a7eb1478be28b5034ba2dd4483fc17a3b28
Muhammed Salman <muhammedsalman181818@gmail.com>
2025-02-02 20:03:27 +0530

    hit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  const t = "myreactapp";
+  return (
+    <div className="App">
+      <div className="content">
+        <h1>{t}</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
